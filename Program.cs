3844a4002aff834f8964721df8abff08ac7fe8c5
Alex <alaurencena.edt@gmail.com>
2023-06-12 16:22:17 +0200

    Deja estela, no borra

--- Program.cs
+++ Program.cs
@@ -16,9 +16,11 @@ namespace SnakeGame
         public static int[] snkTailPos = new int[2];
         public static int points;
         public static int rows;
+        public static int bodyCount = 0;
 
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             ShowMenu();
             while (inGame)
             {
@@ -41,11 +43,15 @@ namespace SnakeGame
                "|_______/    |__| \\__| /__/     \\__\\ |__|\\__\\ |_______|");
 
             Console.WriteLine("Introduce el numero de lineas que quieras");
-            int lines;
-            while (!int.TryParse(Console.ReadLine(), out lines))
+            int lines = 0;
+            while (lines % 2 == 0 || lines < 0 ) 
             {
-                while (lines % 2 == 0 )
-                    Console.WriteLine("Porfavor introduce un numero impar");
+                while (!int.TryParse(Console.ReadLine(), out lines)) 
+                {
+                    Console.WriteLine("introduce numero");
+                }
+                if(lines % 2 == 0 || lines < 0)
+                    Console.WriteLine("Porfavor introduce un numero impar positvo");
             }
 
                     
@@ -53,6 +59,9 @@ namespace SnakeGame
             board = new char[lines, lines];
             snkHeadPos[0] = lines/2;
             snkHeadPos[1] = lines/2;
+            snkTailPos[0] = lines/2;
+            snkTailPos[1] = lines/2;
+
             lines -= 1;
             inGame = true;
             points = 0;
@@ -69,48 +78,125 @@ namespace SnakeGame
 
         private static void CellCheck()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private static void HeadMovement (String teclaPulsada)
+        {
+            int[] previousPos = snkHeadPos;
+            switch (teclaPulsada)
+            {
+                case "Abajo":
+                    if (board[previousPos[0], previousPos[1]] == board[rows, previousPos[1]])
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[0] = 0;
+                    }
+                    else
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[0] += 1;                        
+                    }
+                    break;
+                case "Arriba":
+                    if (board[previousPos[0], previousPos[1]] == board[0, previousPos[1]])
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[0] = rows;
+                    }
+                    else
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[0] -= 1;
+                    }
+                    break;
+
+                case "Derecha":
+                    if (board[previousPos[0], previousPos[1]] == board[previousPos[0], rows])
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[1] = 0;
+                    }
+                    else
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[1] += 1;
+                    }
+                    break;
+                case "Izquierda":
+                    if (board[previousPos[0], previousPos[1]] == board[previousPos[0], 0])
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[1] = rows;
+                    }
+                    else
+                    {
+                        board[previousPos[0], previousPos[1]] = '■';
+                        snkHeadPos[1] -= 1;
+                    }
+                    break;
+            }
+        }
+        private static void TailMovement(String teclaPulsada)
+        {
+            int[] previousPos = snkTailPos;
+            switch (teclaPulsada)
+            {
+                case "Abajo":
+                    board[previousPos[0], previousPos[1]] = ' ';
+                    snkTailPos[0] += 1;
+                    break;
+
+                case "Arriba":
+                    board[previousPos[0], previousPos[1]] = ' ';
+                    snkTailPos[0] -= 1;
+                    break;
+
+                case "Derecha":
+                    board[previousPos[0], previousPos[1]] = ' ';
+                    snkTailPos[1] += 1;
+                    break;
+
+                case "Izquierda":
+                    board[previousPos[0], previousPos[1]] = ' ';
+                    snkTailPos[1] -= 1;
+                    break;
+            }
         }
 
         private static void ReadInput()
         {
-            bool teclaCorrecta = false;
-            //Console.WriteLine("Recuerda, te mueves con las flechas ;)");
-
-            while (!teclaCorrecta) 
-            { 
-        
-                switch (Console.ReadKey().Key)
-                {
-                        case ConsoleKey.DownArrow:
-                            snkHeadPos[0] += 1;
-                            teclaCorrecta = true;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            snkHeadPos[0] -= 1;
-                            teclaCorrecta = true;
-                            break;
-                        case ConsoleKey.LeftArrow:
-                            snkHeadPos[1] -= 1;
-                            teclaCorrecta = true;
-                            break;
-                        case ConsoleKey.RightArrow:
-                            snkHeadPos[1] += 1;
-                            teclaCorrecta = true;
-                            break;
-                        default:
-                        Console.WriteLine("Te he dicho las flechas merluzo!! >:(");
-                        break;
-                }
-
+            Console.WriteLine("Recuerda, te mueves con las flechas ;)");
+            string tecla = "";
+           
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.DownArrow:
+                    tecla = "Abajo";
+                    break;
+                case ConsoleKey.UpArrow:
+                    tecla = "Arriba";
+                    break;
+                case ConsoleKey.LeftArrow:
+                    tecla = "Izquierda";
+                    break;
+                case ConsoleKey.RightArrow:
+                    tecla = "Derecha";
+                    break;                   
             }
+            HeadMovement(tecla);
+
+            if (bodyCount > 3)
+                TailMovement(tecla);
+            else
+                bodyCount++;
         }
- 
+
+        
         private static void PrintBoard()
         {
             Console.WriteLine("Points: " + points);
-            board[snkHeadPos[0], snkHeadPos[1]] = '@';
-
+            board[snkHeadPos[0], snkHeadPos[1]] = '■';
             Console.Write("╔");
             for (int i = 0; i <= rows * 2 + 1; i++)
             {
